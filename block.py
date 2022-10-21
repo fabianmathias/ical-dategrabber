b81fdc0da65a23cbd3362762a3e5ede57992812e
fabianmathias <115463514+fabianmathias@users.noreply.github.com>
2022-10-21 13:14:01 +0200

    Create block.py

---
+++ block.py
@@ -0,0 +1,23 @@
+import requests
+import pandas
+from icalendar import Calendar
+
+urls = ['https://abo.kleiner-kalender.de/ical/c48-feiertage-in-deutschland.ics']
+
+def make_event_list(url: str) -> list:
+    event_list = []
+    calendar_string = Calendar.from_ical(requests.get(url).text)
+    for event in calendar_string.walk("VEVENT"):
+            for i in pandas.date_range(start=event.get('dtstart').dt,end=event.get('dtend').dt,freq='d'):
+                event_list.append(i.strftime('%Y-%m-%d'))
+    return event_list
+
+
+def blockdates():
+    events = []
+    for url in urls:
+        try:
+            events += make_event_list(url)
+        except:
+            print("Damaged Calendar: " + url)
+    return events
